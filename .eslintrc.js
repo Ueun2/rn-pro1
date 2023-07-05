4d68a9063ccc6ff57c66bd27031c420c424ebfb4
yoooo <ghddbtl@gmail.com>
2023-07-05 16:35:10 +0900

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,7 +28,7 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: 'tsconfig.json',
+    project: ["./tsconfig.json"] ,
     tsconfigRootDir: __dirname,
   },
   plugins: ["import",'react', 'react-hooks', '@typescript-eslint', 'prettier'],
